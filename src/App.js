9234c4b4e2cc64535edd4c152ad721a8002ac6da
Santhosh020104 <santhoshofficial62@gmail.com>
2024-10-27 22:05:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { UserProvider } from './UserContext';
+import UserList from './UserList';
+import UserForm from './UserForm';
+import UserEdit from './UserEdit';
+import NotFound from './NotFound';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<UserList />} />
+          <Route path="/users" element={<UserList />} />
+          <Route path="/users/create" element={<UserForm />} />
+          <Route path="/users/edit/:id" element={<UserEdit />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </UserProvider>
   );
-}
+};
 
 export default App;
